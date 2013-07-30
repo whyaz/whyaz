379592598ddbc21d8c63ae547d81a559ede0b4ba
Lynn Fisher <lynn.fisher@meltmedia.com>
2013-07-30 11:13:39 -0700

    updated amazon bucket

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
         key: process.env.AWS_ACCESS_KEY_ID,
         secret: process.env.AWS_SECRET_ACCESS_KEY,
         access: 'public-read',
-        bucket: 'melt-upload'
+        bucket: 'whyaz'
       },
       dev: {
         // These options override the defaults
