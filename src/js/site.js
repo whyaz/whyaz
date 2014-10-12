919068ea0324c160e6893ff609c7947189899353
Jake Heun <jake.heun@meltmedia.com>
2014-10-11 19:37:43 -0700

    Upped the amount of social feed items that get pulled in.

--- src/js/site.js
+++ src/js/site.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
     // TWITTER
     twitter:{
       accounts: ['#whyaz'],
-      limit: 2,
+      limit: 20,
       consumer_key: 'DUtm3KoTGnsKQ9v09seWlYWqP', // make sure to have your app read-only
       consumer_secret: '9PiruhZGrZz3QkZT31gbps52jcuyUkBbQSDccT95r7uJyiPOY1', // make sure to have your app read-only
     },
     // INSTAGRAM
     instagram:{
       accounts: ['#whyaz'],
-      limit:2,
+      limit: 20,
       client_id: '922c1c27ecc8401b9dff1215013efe8b'
     },
     // GENERAL SETTINGS
