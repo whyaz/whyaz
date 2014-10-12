aa7b2bb62245be39c3e080e0cdd0c6272a627455
Jake Heun <jake.heun@meltmedia.com>
2014-10-11 18:22:15 -0700

    Added in basic social feed implementation. This needs to be cleaned up.

---
+++ src/js/site.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('.social-feed-container').socialfeed({
+    // TWITTER
+    twitter:{
+      accounts: ['#whyaz'],
+      limit: 2,
+      consumer_key: 'DUtm3KoTGnsKQ9v09seWlYWqP', // make sure to have your app read-only
+      consumer_secret: '9PiruhZGrZz3QkZT31gbps52jcuyUkBbQSDccT95r7uJyiPOY1', // make sure to have your app read-only
+    },
+    // INSTAGRAM
+    instagram:{
+      accounts: ['#whyaz'],
+      limit:2,
+      client_id: '922c1c27ecc8401b9dff1215013efe8b'
+    },
+    // GENERAL SETTINGS
+    length:400,
+    show_media:true,
+    // Moderation function - if returns false, template will have class hidden
+    //moderation: function(content){
+    //    return  (content.text) ? content.text.indexOf('fuck') == -1 : true;
+    //},
+    update_period: 30000,
+    template: "/lib/social-feed/template.html"
+  });
+});
