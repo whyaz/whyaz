d67c36838446314726c04ff2cba9608906b3c840
Nick Crohn <nick.crohn@gmail.com>
2014-10-13 21:59:21 -0700

    Added in failure/error logic

--- src/js/site.js
+++ src/js/site.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+
+  var isSending = false;
+  var SENT_ERROR_MESSAGE = "Sorry, it appears there was an issue sending your message. Try us again later?";
+  var NO_IDENTITY_MESSAGE = "An email or twitter handle is required.";
+  var NO_MESSAGE_ERROR = "A message is required.";
+
   $('.social-feed-container').socialfeed({
     // TWITTER
     twitter:{
@@ -24,9 +30,26 @@ $(document).ready(function(){
     template: "/public/js/templates/feedElement.html"
   });
 
+  function isValid(data) {
+    var ret = true;
+
+    if (!data.email && !data.twitter) {
+      ret = false;
+    }
+
+    if (!data.message || data.message.length < 1) {
+      ret = false;
+    }
+
+    return ret;
+  }
+
   $('form button[type="submit"]').click(
     function(event) {
       event.preventDefault();
+      if (isSending) return;
+      // This is for those silly robots
+      if ($('form input[name="email2"]').val()) return;
 
       var data = {
         name: $('form input[name="name"]').val(),
@@ -35,14 +58,42 @@ $(document).ready(function(){
         message: $('form textarea[name="message"]').val()
       };
 
-      $.ajax({
-        url: 'http://localhost:8081/send-message',
-        type: 'post',
-        data: data,
-        success: function(data, status) {
-          console.log(data);
+      if (data.twitter && data.twitter.indexOf('@') < 0) {
+        data.twitter = '@'+data.twitter;
+      }
+
+      if (isValid(data)) {
+        $.ajax({
+          url: 'http://localhost:8081/send-message',
+          type: 'post',
+          data: data,
+          complete: function (data) {
+            isSending = false;
+
+            $('form button[type="submit"]').toggleClass('sending', isSending);
+            $('.error').toggleClass('hidden', data.status === 200);
+            $('.success').toggleClass('hidden', data.status !== 200);
+
+            if (data.status >= 400 && data.status <= 599) {
+              $('.error').html(SENT_ERROR_MESSAGE);
+            }
+          }
+        });
+
+        isSending = true;
+        $('form button[type="submit"]').toggleClass('sending', isSending);
+      } else {
+        console.log(isValid(data));
+        $('.error').toggleClass('hidden', isValid(data));
+
+        if (!data.message) {
+          $('.error').html(NO_MESSAGE_ERROR);
+        } else {
+          $('.error').html(NO_IDENTITY_MESSAGE);
         }
-      });
+
+      }
+
     });
 
 });
