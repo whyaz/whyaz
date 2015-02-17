803615de4791a15c116c18c730690a9994f76566
Jake Heun <jake.heun@meltmedia.com>
2015-02-17 00:25:02 -0700

    Cleaned up unused js that was subsequently causing bower to fail on deploys. Hooray for dependency versions getting removed from the interwebs.

--- src/public/js/site.js
+++ src/public/js/site.js
@@ -5,31 +5,6 @@ $(document).ready(function(){
   var NO_IDENTITY_MESSAGE = "An email or twitter handle is required.";
   var NO_MESSAGE_ERROR = "A message is required.";
 
-  $('.social-feed-container').socialfeed({
-    // TWITTER
-    twitter:{
-      accounts: ['#whyaz -RT -DesertHorizonQC'],
-      limit: 20,
-      consumer_key: 'DUtm3KoTGnsKQ9v09seWlYWqP', // make sure to have your app read-only
-      consumer_secret: '9PiruhZGrZz3QkZT31gbps52jcuyUkBbQSDccT95r7uJyiPOY1', // make sure to have your app read-only
-    },
-    // INSTAGRAM
-    instagram:{
-      accounts: ['#whyaz'],
-      limit: 20,
-      client_id: '922c1c27ecc8401b9dff1215013efe8b'
-    },
-    // GENERAL SETTINGS
-    length:400,
-    show_media:true,
-    // Moderation function - if returns false, template will have class hidden
-    moderation: function(content){
-        return  (content.text) ? content.text.toLowerCase().indexOf('whyaz') > -1 : true;
-    },
-    update_period: 30000,
-    template: "/public/js/templates/feedElement.html"
-  });
-
   function isValid(data) {
     var ret = true;
 
