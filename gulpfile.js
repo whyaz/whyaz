4dd2900d10ebd5b9146ea83c07518f0e8f77050f
Lynn Fisher <lynnfishe@gmail.com>
2019-04-30 09:25:42 -0700

    put redirects into dist

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ var url          = require('url');
 
 function copy() {
 
-  return gulp.src(['src/**/*.*', '!src/templates/**/*.*', '!src/views/**/*.*', '!src/styl/**/*.*'])
+  return gulp.src(['src/**/*.*', '_redirects', '!src/templates/**/*.*', '!src/views/**/*.*', '!src/styl/**/*.*'])
     .pipe(gulp.dest('dist/'));
 
 }
